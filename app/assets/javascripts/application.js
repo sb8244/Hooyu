4d42ca58230845d4bad2ebd12409c2441728caf4
Stephen Bussey <steve.bussey@salesloft.com>
2015-06-22 00:32:11 -0400

    pre-filter to include the CSRF token. $.ajaxSetup isn't taking for some reason.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require react_ujs
 //= require components
 //= require_tree .
+
+$.ajaxPrefilter(function( options ) {
+  if ( !options.beforeSend) {
+    options.beforeSend = function (xhr) {
+      xhr.setRequestHeader('X-CSRF-Token',  $('meta[name="csrf-token"]').attr('content'));
+    }
+  }
+});
