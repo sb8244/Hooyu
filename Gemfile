de28a149ea3ebb04113b4d2f500ccb9733329518
Stephen Bussey <steve.bussey@salesloft.com>
2015-06-27 22:48:23 -0400

    remove not needed gems and fix the breakage

--- Gemfile
+++ Gemfile
@@ -19,19 +19,6 @@ group :development, :test do
   gem 'pry'
 end
 
-# front end
-gem 'therubyracer'
-gem 'jquery-rails'
-gem 'slim-rails'
-gem 'bootstrap-sass', '~> 3.3.3'
-gem 'font-awesome-sass', '~> 4.3.0'
-gem 'sass-rails', '>= 3.2'
-gem 'uglifier', '>= 2.4.0'
-
-gem 'sprockets', '>= 3.0.0'
-gem 'sprockets-es6'
-gem 'react-rails', '~> 1.0'
-
 # db
 gem 'sqlite3'
 gem 'neo4j', '~> 4.1.1'
@@ -42,6 +29,5 @@ gem 'dotenv-rails'
 gem 'responders', '~> 2.0'
 
 # Authentication
-gem 'devise'
 gem 'omniauth'
-gem 'omniauth-twitter'
+gem 'omniauth-google_oauth2'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def react_component(name, props = {}, options = {}, &block)
+    options = {:tag => options} if options.is_a?(Symbol)
+
+    html_options = options.reverse_merge(:data => {})
+    html_options[:data].tap do |data|
+      data[:react_class] = name
+      data[:react_props] = (props.is_a?(String) ? props : props.to_json)
+    end
+    html_tag = html_options[:tag] || :div
+
+    # remove internally used properties so they aren't rendered to DOM
+    html_options.except!(:tag, :prerender)
+
+    content_tag(html_tag, '', html_options, &block)
+  end
 end
